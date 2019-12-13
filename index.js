67687477473a4fb92e02269295d3554304d88d18
soumyasourabha <soumyasourabha.608@gmail.com>
2019-12-13 17:28:04 +0530

    pushing to heroku

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = express();
-const port = Process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 require('dotenv/config')
-const cors = require('cors');
-
-app.use(cors)
 
 //body-parser
 app.use(express.json())
@@ -22,7 +19,7 @@ const commentRoute = require('./routes/Comments');
 app.use('/posts/post',commentRoute)
 
 //db connection
-mongoose.connect(Process.env.DB_CONNECTION,
+mongoose.connect(process.env.DB_CONNECTION,
                 {useNewUrlParser : true},
                 () => {console.log('DB Connected !')})
 
